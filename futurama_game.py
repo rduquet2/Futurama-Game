d59e8c92ea6659c4704e31c8457bf5648d9c27ce
Ryan <duquet.ryan@gmail.com>
2021-07-06 23:05:39 -0700

    Created pause and play buttons. Drew background to screen.

--- futurama_game.py
+++ futurama_game.py
@@ -2,9 +2,19 @@ import sys, pygame, random
 
 pygame.init()
 
-size = width, height = 750, 750
+size = width, height = 706, 540
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Planet Express Delivery")
+background_image = pygame.image.load("./background.jpg").convert()
+
+# buttons
+play_button_image = pygame.image.load("./play_button.png")
+play_button_image = pygame.transform.scale(play_button_image, (40, 40))
+play_button_rect = play_button_image.get_rect(center = (675, 30))
+pause_button_image = pygame.image.load("./pause_button.png")
+pause_button_image = pygame.transform.scale(pause_button_image, (40, 40))
+pause_button_rect = pause_button_image.get_rect(center = (675, 30))
+
 SHIP_VELOCITY = 6
 SHIP_LASER_COOLDOWN_SPEED = 450
 ALIEN_LASER_COOLDOWN_SPEED = 1000
@@ -21,14 +31,14 @@ class PlanetExpressShip(pygame.sprite.Sprite):
 
         self.image = pygame.image.load("./planet_express_ship.png")
         self.image = pygame.transform.scale(self.image, (100, 100))
-        self.rect = self.image.get_rect(center = (350, 350))
+        self.rect = self.image.get_rect(center = (155, 180))
 
     def create_laser(self):
         return PlanetExpressShipLaser(self.rect.x + 70, self.rect.y + 35)    
 
 class PlanetExpressShipLaser(pygame.sprite.Sprite):
     def __init__(self, x, y):
-        super().__init__()
+        pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load("./purple_laser.png")
         self.image = pygame.transform.scale(self.image, (70, 110))
@@ -38,36 +48,76 @@ class PlanetExpressShipLaser(pygame.sprite.Sprite):
         # update x position
         self.rect.x += 5
         # If we are past the right boundary, kill self
-        if self.rect.x >= width:
+        if self.rect.x > width:
             self.kill()
+
+class Alien(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image.load("/.aliens.png")
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.image = pygame.transform.flip(self.image, True, False)
+        self.rect = self.image.get_rect(center = (width + 10, random.uniform(0, height)))
+
+    # def update(self):
+
 
 class Asteroid(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__(self)
+        pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load("./asteroid.png")
-        self.image = pygame.transform.scale(self.image, (5, 5))
-        self.rect = self.image.get_rect(center = (width + 10, random.uniform()))
+        self.image = pygame.transform.scale(self.image, (75, 75))
+        # generate asteroid in right boundary randomly
+        self.rect = self.image.get_rect(center = (width + 10, random.uniform(0, height)))
+        self.x_speed = random.uniform(-10, -5)
+        self.y_speed = random.uniform(-10, 10)
 
+    def update(self):
+        self.rect = self.rect.move(self.x_speed, self.y_speed)
+
+        if self.rect.bottom > height or self.rect.top < 0:
+            self.y_speed = self.y_speed * -1
+
+        if self.rect.right < 0:
+            self.kill()
 
 ship = PlanetExpressShip()
 ship_sprite = pygame.sprite.Group()
 ship_sprite.add(ship)
 
 ship_laser_sprite = pygame.sprite.Group()
+asteroid_sprite = pygame.sprite.Group()
+
+# states to keep track of played or paused
+button_image = pause_button_image
+button_rect = pause_button_rect
+paused = False
 
 clock = pygame.time.Clock()
 while 1:
     clock.tick(60)
     screen.fill((0, 0, 0))
+    screen.blit(background_image, [0, 0])
+    screen.blit(button_image, button_rect)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pos() >= (664, 16) and pygame.mouse.get_pos() <= (685, 34):
+                paused = not paused
+                if paused == False:
+                    button_image = pause_button_image
+                    button_rect = pause_button_rect
+                else:
+                    button_image = play_button_image
+                    button_rect = play_button_rect  
         elif event.type == ship_laser_shot_event:
             # when the timeout of LASER_COOLDOWN_SPEED is over, reset it
             ship_laser_cooldown_finished = True
-            pygame.time.set_timer(ship_laser_shot_event, 0) 
+            pygame.time.set_timer(ship_laser_shot_event, 0)    
 
     # handle WASD or arrow key movement
     keys = pygame.key.get_pressed()
@@ -100,6 +150,6 @@ while 1:
         
     ship_laser_sprite.draw(screen)
     ship_laser_sprite.update()
-    ship_sprite.draw(screen)   
+    ship_sprite.draw(screen)
 
     pygame.display.flip()
